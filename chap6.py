1b1fdb34d2f52f5f8274f47cc0cb528113320947
Kelly Chiang <chiang.kellymimi@gmail.com>
2013-05-02 14:22:16 -0400

    Chapter 6 & 7 exercises
    Need help~

--- chap6.py
+++ chap6.py
@@ -1,5 +1,5 @@
 # Enter your answrs for chapter 6 here
-# Name:
+# Name: Kelly Chiang
 
 
 # Ex. 6.6
@@ -7,3 +7,17 @@
 
 
 # Ex 6.8
+#The greatest common divisor (GCD) of a and b is the largest number that divides both of them with no remainder.
+#One way to find the GCD of two numbers is Euclid’s algorithm, which is based on the observation that if r is the remainder when a is divided by b, then gcd(a, b) = gcd(b, r). As a base case, we can use gcd(a, 0) = a.
+#Write a function called gcd that takes parameters a and b and returns their greatest common divisor. If you need help, see http: // en. wikipedia. org/ wiki/ Euclidean_ algorithm .
+
+def gcd(a, b):
+    if a % b == 0;
+    and b > a
+        return b
+
+#stackoverflow solution -- can someone explain?
+def gcd(a, b):
+    while b:
+        a, b = b, a%b
+    return a
--- chap7.py
+++ chap7.py
@@ -1,9 +1,53 @@
 # Enter your answrs for chapter 7 here
-# Name:
+# Name: Kelly Chiang
 
+#NOTES
+a = 4.0 #example from previous section
+x = 3.0
 
+x = y
+y = (x + a/x) / 2
+while True:
+    print x
+    y = (x + a/x) / 2
+    if y == x:
+        break 
+    x = y
+
+    
 # Ex. 7.5
 
+# Formula 1 = 2√2 ∑∞ (4k)!(1103 + 26390k) π 9801 k=0 (k!)43964k
+#solution for 7.5: Need help with forming thoughts to create this in the future o_o (understand the pieces, but not sure how i would create this on my own!!)
+import math
 
+def factorial(n):
+    """Computes factorial of n."""
+    if n == 0:
+        return 1
+    else:
+        recurse = factorial(n-1)
+        result = n * recurse
+        return result
+
+def estimate_pi():
+    """Computes an estimate of pi.
+
+    Algorithm due to Srinivasa Ramanujan, from 
+    http://en.wikipedia.org/wiki/Pi
+    """
+    total = 0
+    k = 0
+    factor = 2 * math.sqrt(2) / 9801
+    while True:
+        num = factorial(4*k) * (1103 + 26390*k)
+        den = factorial(k)**4 * 396**(4*k)
+        term = factor * num / den
+        total += term
+        
+        if abs(term) < 1e-15: break
+        k += 1
+
+    return 1 / total
 
 # How many iterations does it take to converge?
